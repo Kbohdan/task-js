4373ce020c57071e3f9067c3357afc79468ed440
Kbohdan <79533106+Kbohdan@users.noreply.github.com>
2021-02-23 19:59:43 +0200

    task-2
    Запросить у пользователя число от 0 до 9 и вывести ему спецсимвол, который расположен на этой клавише (1–!, 2–@, 3–# и т. д).

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let age = +prompt("Введите ваш возраст");
+let num = +prompt('Введите число от 0-9');
 
-if (age < 2) {
-    alert("Вы ребенок");
-
-} else if (age < 18) {
-    alert("Вы подросток");
-} else if (age < 60) {
-    alert("Вы взрослый");
-} else if (age > 60) {
-    alert("Вы пенсионер");
+if (num == 1) {
+    alert("!");
+} else if (num == 2) {
+    alert("@");
+} else if (num == 3) {
+    alert("#");
+} else if (num == 4) {
+    alert("$");
+} else if (num == 5) {
+    alert("%");
+} else if (num == 6) {
+    alert("^");
+} else if (num == 7) {
+    alert("&");
+} else if (num == 8) {
+    alert("*");
+} else if (num == 9) {
+    alert("(");
+} else if (num == 0) {
+    alert(")");
+} else {
+    alert("Не правильное число");
 }
